d4f5ebeaad337d18bcd3a175007edd68064e5983
tyange <usun16@gmail.com>
2022-12-28 08:01:35 +0900

    onClickCountryButtonHandler

--- src/components/NewsOptions/CountryOptions/CountryOption/CountryOption.tsx
+++ src/components/NewsOptions/CountryOptions/CountryOption/CountryOption.tsx
@@ -3,11 +3,18 @@ import { Country } from "../../../../types/country";
 
 type CountryOptionProps = {
   country: Country;
+  onClickCountryButtonHandler: (countryCode: string) => void;
 };
 
-const CountryOption = ({ country }: CountryOptionProps) => {
+const CountryOption = ({
+  country,
+  onClickCountryButtonHandler,
+}: CountryOptionProps) => {
   return (
-    <button class="w-40 h-16 shadow-md border self-center justify-self-center px-5 py-2 flex flex-col items-center rounded-xl">
+    <button
+      class="w-40 h-16 shadow-md border self-center justify-self-center px-5 py-2 flex flex-col items-center rounded-xl"
+      onClick={() => onClickCountryButtonHandler(country.code)}
+    >
       <span>{getFlagEmoji(country.code)}</span>
       <span>{country["kor-name"]}</span>
     </button>
--- src/components/NewsOptions/CountryOptions/CountryOptions.tsx
+++ src/components/NewsOptions/CountryOptions/CountryOptions.tsx
@@ -1,12 +1,30 @@
-import { Component } from "solid-js";
+import { Component, createSignal } from "solid-js";
 import { countries } from "../../../constants/countries";
 import CountryOption from "./CountryOption/CountryOption";
 
 const CountryOptions: Component = () => {
+  const [countryList, setCountryList] = createSignal(countries);
+  const onClickCountryButtonHandler = (countryCode: string) => {
+    const filteredCountries = countries.filter(
+      (country) => country.code === countryCode
+    );
+
+    setCountryList(filteredCountries);
+  };
+
   return (
-    <div class="grid grid-cols-5 px-52 py-10 gap-5">
-      {countries.map((country) => (
-        <CountryOption country={country} />
+    <div
+      class={
+        countryList().length > 1
+          ? "grid grid-cols-5 px-52 py-10 gap-5"
+          : "flex justify-center items-center p-10"
+      }
+    >
+      {countryList().map((country) => (
+        <CountryOption
+          country={country}
+          onClickCountryButtonHandler={onClickCountryButtonHandler}
+        />
       ))}
     </div>
   );
