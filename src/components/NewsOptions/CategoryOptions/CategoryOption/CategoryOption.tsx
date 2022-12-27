f49f24914d3c07a571d999db2800c0f4e248f20a
tyange <usun16@gmail.com>
2022-12-28 08:13:58 +0900

    onClickCategoryButtonHandler

--- src/components/NewsOptions/CategoryOptions/CategoryOption/CategoryOption.tsx
+++ src/components/NewsOptions/CategoryOptions/CategoryOption/CategoryOption.tsx
@@ -2,10 +2,17 @@ import { Category } from "../../../../types/category";
 
 type CategoryOptionProps = {
   category: Category;
+  onClickCategoryButtonHandler: (categoryName: string) => void;
 };
-const CategoryOption = ({ category }: CategoryOptionProps) => {
+const CategoryOption = ({
+  category,
+  onClickCategoryButtonHandler,
+}: CategoryOptionProps) => {
   return (
-    <button class="w-24 h-16 shadow-md border flex flex-col items-center justify-center rounded-xl">
+    <button
+      class="w-24 h-16 shadow-md border flex flex-col items-center justify-center rounded-xl"
+      onClick={() => onClickCategoryButtonHandler(category.name)}
+    >
       {category["kor-name"]}
     </button>
   );
--- src/components/NewsOptions/CategoryOptions/CategoryOptions.tsx
+++ src/components/NewsOptions/CategoryOptions/CategoryOptions.tsx
@@ -1,13 +1,31 @@
-import { Component } from "solid-js";
+import { Component, createSignal } from "solid-js";
 import CategoryOption from "./CategoryOption/CategoryOption";
 
 import { categories } from "../../../constants/category";
 
 const CategoryOptions: Component = () => {
+  const [categoryList, setCategoryList] = createSignal(categories);
+  const onClickCategoryButtonHandler = (categoryName: string) => {
+    const filteredCategories = categoryList().filter(
+      (category) => category.name === categoryName
+    );
+
+    setCategoryList(filteredCategories);
+  };
+
   return (
-    <div class="flex justify-center items-center gap-5 p-10">
-      {categories.map((category) => (
-        <CategoryOption category={category} />
+    <div
+      class={
+        categoryList().length > 1
+          ? "flex justify-center items-center gap-5 p-10"
+          : "flex justify-center items-center p-10"
+      }
+    >
+      {categoryList().map((category) => (
+        <CategoryOption
+          category={category}
+          onClickCategoryButtonHandler={onClickCategoryButtonHandler}
+        />
       ))}
     </div>
   );
