5e4f5aaaa87a6106e61a00818115c8779b65195e
tyange <usun16@gmail.com>
2022-12-30 22:58:27 +0900

    divider & submit button 추가

--- src/components/NewsOptions/NewsOptions.tsx
+++ src/components/NewsOptions/NewsOptions.tsx
@@ -1,15 +1,31 @@
 import { Component } from "solid-js";
 import CountryOptions from "./CountryOptions/CountryOptions";
 import CategoryOptions from "./CategoryOptions/CategoryOptions";
+import selectedCountry from "../../store/selectedCountry";
+import selectedCategory from "../../store/selectedCategory";
+import SubmitButtons from "./SubmitButtons/SubmitButtons";
 
 const NewsOptions: Component = () => {
+  const { country } = selectedCountry;
+  const { category } = selectedCategory;
+
+  const divider = () => (
+    <div class="w-full flex justify-center items-center">
+      <div class="w-1/4 border-t-2"></div>
+    </div>
+  );
+
   return (
     <div>
       <CountryOptions />
-      <div class="w-full flex justify-center items-center">
-        <div class="w-1/4 border-t-2"></div>
-      </div>
+      {divider()}
       <CategoryOptions />
+      {country() && category() && (
+        <>
+          {divider()}
+          <SubmitButtons />
+        </>
+      )}
     </div>
   );
 };
