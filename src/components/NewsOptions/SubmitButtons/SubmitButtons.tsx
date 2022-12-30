4b472fe0cee8e1203175976811b723ef936808de
tyange <usun16@gmail.com>
2022-12-30 22:58:07 +0900

    git rm --cached -r

---
+++ src/components/NewsOptions/SubmitButtons/SubmitButtons.tsx
@@ -0,0 +1,11 @@
+const SubmitButtons = () => {
+  return (
+    <div class="w-full flex justify-center items-center p-10">
+      <button class="border shadow-md p-5 rounded-xl bg-sky-100 hover:bg-sky-200 transition-all">
+        뉴스 보기
+      </button>
+    </div>
+  );
+};
+
+export default SubmitButtons;
