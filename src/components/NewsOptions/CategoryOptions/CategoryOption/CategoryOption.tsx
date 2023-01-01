2ae3260af36bcb824f61127b7b98527d849ccfc5
tyange <usun16@gmail.com>
2023-01-01 14:54:46 +0900

    categoryOption text-size 조정

--- src/components/NewsOptions/CategoryOptions/CategoryOption/CategoryOption.tsx
+++ src/components/NewsOptions/CategoryOptions/CategoryOption/CategoryOption.tsx
@@ -10,12 +10,9 @@ const CategoryOption = ({
   onClickCategoryButtonHandler,
   onClickCategoryInit,
 }: CategoryOptionProps) => {
-  const buttonClass =
-    "w-20 h-16 sm:24 shadow-md border flex flex-col items-center justify-center rounded-xl";
-
   return (
     <button
-      class={buttonClass}
+      class="w-20 h-16 sm:24 shadow-md border flex flex-col items-center justify-center rounded-xl text-xs sm:text-sm"
       onClick={
         onClickCategoryButtonHandler
           ? () => onClickCategoryButtonHandler(category)
