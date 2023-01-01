cf48586c4c956bcaf5ab0114c7810995148e16f7
tyange <usun16@gmail.com>
2023-01-01 10:54:27 +0900

    ArticleItem 반응형 스타일링

--- src/components/Articles/Article/ArticleItem.tsx
+++ src/components/Articles/Article/ArticleItem.tsx
@@ -5,7 +5,7 @@ type ArticleItemProps = {
 };
 const ArticleItem = ({ article }: ArticleItemProps) => {
   return (
-    <li class="w-80 sm:w-96 md:w-4/5">
+    <li class="w-80 sm:w-3/5 md:w-4/5">
       <a
         href={article.url ? article.url : ""}
         class="w-full block shadow-md border rounded-xl p-10 hover:shadow-lg hover:underline transition-all"
