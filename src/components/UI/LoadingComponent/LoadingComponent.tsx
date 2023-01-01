ad385af350c22ab89fc90650cee89d628aab5bf3
tyange <usun16@gmail.com>
2023-01-01 17:22:37 +0900

    LoadingComponent 스타일링 조정

--- src/components/UI/LoadingComponent/LoadingComponent.tsx
+++ src/components/UI/LoadingComponent/LoadingComponent.tsx
@@ -4,11 +4,11 @@ const LoadingComponent = () => {
   return (
     <div class="w-full h-full flex justify-center items-center relative">
       <img
-        class="w-3/5 animate-spin-slow"
+        class="w-3/5 animate-spin-slow md:w-2/5"
         src={expandingCircle}
         alt="확장하는 원"
       />
-      <div class="absolute w-32 sm:w-52 md:w-2/5">
+      <div class="absolute w-32 sm:w-52 md:w-1/5">
         <img class="w-full" src={expandingCircleInPerson} alt="확장하는 원" />
       </div>
     </div>
